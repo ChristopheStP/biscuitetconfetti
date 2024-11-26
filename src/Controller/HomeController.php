e263c29f7a27648ef58d5ae78d19c2ab1d253a70
ChristopheStP <csaintpierre55@aol.com>
2024-11-26 17:21:38 +0100

    Mails

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,3 +15,4 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig');
     }
 }
+
